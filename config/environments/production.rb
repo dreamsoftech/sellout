3a7a5070939ebfd0f3db2f989b70dcab9d5dfbe5
reedkevin88 <reedkevin88@gmail.com>
2014-03-28 20:16:50 +0400

    modify production email domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Sellout::Application.configure do
     }
 
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'selloutplatform.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
